7283d6670aa93ed2514a6b95d0ee799e5bbef10f
darkyasha14 <shinigaminoirfan@gmail.com>
2020-08-18 13:35:41 +0700

    fetch api category

--- app/src/main/java/com/example/ta_2020/home/HomeFragment.java
+++ app/src/main/java/com/example/ta_2020/home/HomeFragment.java
@@ -1,21 +1,55 @@
 package com.example.ta_2020.home;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Toast;
 
+import com.example.ta_2020.PrefManager;
 import com.example.ta_2020.R;
+import com.example.ta_2020.apihelper.ApiInterface;
+import com.example.ta_2020.apihelper.UtilsApi;
+import com.example.ta_2020.home.adapter.CategoryAdapter;
+import com.example.ta_2020.home.model.Category;
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
+    Context context;
+
+    RecyclerView recyclerView;
+    CategoryAdapter categoryAdapter;
+    List<Category.DataBean> dataBeans;
+
+    PrefManager prefManager;
+
+
+    ApiInterface apiHelper;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -28,6 +62,63 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
+        context = view.getContext();
+        apiHelper = UtilsApi.getApiService();
+        recyclerView = view.findViewById(R.id.recyclerGroup);
+
+        prefManager = new PrefManager(view.getContext());
+        
+        fetchCategory();
+        
         return view;
+    }
+
+    private void fetchCategory() {
+        apiHelper.getCategory(prefManager.getTokenUser()).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        if (jsonObject.getString("code").equals("1")) {
+
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+                            dataBeans = new ArrayList<>();
+                            Gson gson = new Gson();
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                Category.DataBean dataBean = gson.fromJson(jsonArray.getJSONObject(i).toString(), Category.DataBean.class);
+                                dataBeans.add(dataBean);
+                            }
+
+                            categoryAdapter = new CategoryAdapter(context, dataBeans);
+                            recyclerView.setAdapter(categoryAdapter);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(context));
+                            recyclerView.setHasFixedSize(true);
+                        } else {
+                            Toast.makeText(context, "404", Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                        Toast.makeText(context, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+            }
+        });
     }
 }
