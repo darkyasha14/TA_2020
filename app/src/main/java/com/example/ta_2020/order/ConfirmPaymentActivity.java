952b47305e5ba7e33e6a207ab1af38b36b6dea55
darkyasha14 <shinigaminoirfan@gmail.com>
2020-08-21 22:20:15 +0700

    Create ConfirmPaymentActivity.java

---
+++ app/src/main/java/com/example/ta_2020/order/ConfirmPaymentActivity.java
@@ -0,0 +1,16 @@
+package com.example.ta_2020.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ta_2020.R;
+
+public class ConfirmPaymentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_c_onfirm_payment);
+    }
+}
