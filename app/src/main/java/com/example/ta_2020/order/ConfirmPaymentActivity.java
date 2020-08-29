a5135a2231d48a95a2eadaa837e3494eddb163ac
darkyasha14 <shinigaminoirfan@gmail.com>
2020-08-29 18:47:58 +0700

    add upload file

--- app/src/main/java/com/example/ta_2020/order/ConfirmPaymentActivity.java
+++ app/src/main/java/com/example/ta_2020/order/ConfirmPaymentActivity.java
@@ -1,12 +1,18 @@
 package com.example.ta_2020.order;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,8 +24,10 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.loader.content.CursorLoader;
 
 import com.example.ta_2020.MainActivity;
 import com.example.ta_2020.PrefManager;
@@ -72,6 +80,9 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
     @BindView(R.id.relativeBottom)
     RelativeLayout relativeBottom;
 
+    Uri mImageUri;
+    String imagePath;
+
     ImageView ibPhoto;
 
     ApiInterface apiInterface;
@@ -83,7 +94,9 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
     DatePickerDialog dialog;
     Context mContext = this;
 
-    private static final int CAMERA_REQUEST = 1888;
+
+
+    private static final int PICK_IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,7 +113,7 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
         ibPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                takeImage();
+                openFileChooser();
             }
         });
 
@@ -130,111 +143,125 @@ public class ConfirmPaymentActivity extends AppCompatActivity {
             }
         });
 
-
-    }
-    private File createTempFile(Bitmap bitmap) {
-        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-                , System.currentTimeMillis() +"_image.webp");
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();
-
-        bitmap.compress(Bitmap.CompressFormat.WEBP,0, bos);
-        byte[] bitmapdata = bos.toByteArray();
-        //write the bytes in file
-
-        try {
-            FileOutputStream fos = new FileOutputStream(file);
-            fos.write(bitmapdata);
-            fos.flush();
-            fos.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return file;
-    }
-
-    private void takeImage() {
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-    }
-    //untuk mengambil gambar hasil capture camera tadi kita harus override onActivityResult dan membaca resultCode apakah sukses dan requestCode apakah dari Camera_Request
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-            Bitmap gambarbitmap = (Bitmap) data.getExtras().get("data");
-            //panggil method uploadImage
-            uploadImage(gambarbitmap);
-
-
-        }
-    }
-
-    private void uploadImage(final Bitmap gambarbitmap) {
-
         btnPaid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                HashMap<String, String> map = new HashMap<>();
-                map.put("Authorization", prefManager.getTokenUser());
-                Map<String, RequestBody> bodyMap = new HashMap<>();
-                bodyMap.put("name", createPartFromString(tvName.getText().toString()));
-                bodyMap.put("email", createPartFromString(tvEmail.getText().toString()));
-                bodyMap.put("payment_date", createPartFromString(txtFormDate.getText().toString() + " "));
-                bodyMap.put("total_price", createPartFromString(idTotalPrice.getText().toString()));
-                bodyMap.put("payment_method", createPartFromString(idPaymentMethod.getText().toString()));
-                bodyMap.put("invoice_no", createPartFromString(idInvoice.getText().toString()));
-                bodyMap.put("description", createPartFromString(tvDesc.getText().toString()));
+                if (imagePath == null){
+                    Toast.makeText(context, "Mohon upload bukti transfer", Toast.LENGTH_SHORT).show();
+                }else{
+                    uploadImage();
+                }
 
-                //convert gambar jadi File terlebih dahulu dengan memanggil createTempFile yang di atas tadi.
-                File file = createTempFile(gambarbitmap);
-                RequestBody reqFile = RequestBody.create(MediaType.parse("multipart/from-data"), file);
-                MultipartBody.Part body = MultipartBody.Part.createFormData("img_pay", file.getName(), reqFile);
-
-                apiInterface.requestConfirmPayment(map, bodyMap, body).enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        if (response.isSuccessful()) {
-                            try {
-                                JSONObject jsonObject = new JSONObject(response.body().string());
-                                if (jsonObject.getString("code").equals("0")) {
-                                    Toast.makeText(mContext, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
-                                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                    intent.putExtra("FLAGPAGE", 1);
-                                    startActivity(intent);
-                                    finish();
-                                }else {
-                                    Toast.makeText(mContext, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
-                                }
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        } else {
-                            try {
-                                JSONObject jsonObject = new JSONObject(response.errorBody().string());
-                                Toast.makeText(mContext, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
-
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable t) {
-                        Log.e("ERROR", String.valueOf(t));
-
-                    }
-                });
             }
         });
 
-
-
     }
+    private void openFileChooser() {
+        final CharSequence[] options = {"Choose from Gallery", "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("Add Photo");
+
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if (options[i].equals("Choose from Gallery")) {
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, PICK_IMAGE_REQUEST);
+
+                } else if (options[i].equals("Cancel")) {
+                    dialogInterface.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            mImageUri = data.getData();
+            Toast.makeText(context, "" + mImageUri, Toast.LENGTH_SHORT).show();
+
+
+            imagePath = getRealPathFromUri(mImageUri);
+
+            ibPhoto.setImageURI(Uri.parse(imagePath));
+
+        }
+    }
+
+    private void uploadImage() {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Authorization", prefManager.getTokenUser());
+        Map<String, RequestBody> bodyMap = new HashMap<>();
+        bodyMap.put("name", createPartFromString(tvName.getText().toString()));
+        bodyMap.put("email", createPartFromString(tvEmail.getText().toString()));
+        bodyMap.put("payment_date", createPartFromString(txtFormDate.getText().toString() + " "));
+        bodyMap.put("total_price", createPartFromString(idTotalPrice.getText().toString()));
+        bodyMap.put("payment_method", createPartFromString(idPaymentMethod.getText().toString()));
+        bodyMap.put("invoice_no", createPartFromString(idInvoice.getText().toString()));
+        bodyMap.put("description", createPartFromString(tvDesc.getText().toString()));
+
+
+        File file = new File(imagePath);
+        RequestBody reqFile = RequestBody.create(MediaType.parse("multipart/from-data"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("img_pay", file.getName(), reqFile);
+
+        apiInterface.requestConfirmPayment(map, bodyMap, body).enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                if (response.isSuccessful()) {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.body().string());
+                        if (jsonObject.getString("code").equals("0")) {
+                            Toast.makeText(mContext, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            intent.putExtra("FLAGPAGE", 1);
+                            startActivity(intent);
+                            finish();
+                        }else {
+                            Toast.makeText(mContext, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response.errorBody().string());
+                        Toast.makeText(mContext, "" + jsonObject.getString("message"), Toast.LENGTH_SHORT).show();
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                Log.e("ERROR", String.valueOf(t));
+
+            }
+        });
+    }
+
+    private String getRealPathFromUri(Uri uri) {
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(context, uri, projection, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_idx = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_idx);
+        cursor.close();
+        return result;
+    }
+
+
 
     @NonNull
     private RequestBody createPartFromString(String descriptionString) {
