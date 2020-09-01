6cb7d4f2b85d3fa26649bf5b8ca9b67070444bb4
darkyasha14 <shinigaminoirfan@gmail.com>
2020-09-01 15:44:44 +0700

    Create PaymentMethodActivity.java

---
+++ app/src/main/java/com/example/ta_2020/order/PaymentMethodActivity.java
@@ -0,0 +1,16 @@
+package com.example.ta_2020.order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ta_2020.R;
+
+public class PaymentMethodActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_method);
+    }
+}
