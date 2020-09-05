52debf2bfd34b40006e64a938e99148381f2ee72
darkyasha14 <shinigaminoirfan@gmail.com>
2020-09-05 21:29:00 +0700

    Update PaymentMethodActivity.java

--- app/src/main/java/com/example/ta_2020/order/PaymentMethodActivity.java
+++ app/src/main/java/com/example/ta_2020/order/PaymentMethodActivity.java
@@ -1,8 +1,10 @@
 package com.example.ta_2020.order;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.example.ta_2020.R;
 
@@ -12,5 +14,23 @@ public class PaymentMethodActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_method);
+
+        Toolbar toolbar = findViewById(R.id.toolbarPay);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Booking Detail");
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // API 5+ solution
+                onBackPressed();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
