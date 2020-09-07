008a066b912311c231acdd8544f631363d10b191
darkyasha14 <shinigaminoirfan@gmail.com>
2020-09-07 19:33:58 +0700

    Update SplashScreenActivity.java

--- app/src/main/java/com/example/ta_2020/SplashScreenActivity.java
+++ app/src/main/java/com/example/ta_2020/SplashScreenActivity.java
@@ -2,8 +2,13 @@ package com.example.ta_2020;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -27,15 +32,54 @@ public class SplashScreenActivity extends AppCompatActivity {
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.setStatusBarColor(Color.TRANSPARENT);
         }
+        if(isWorkingInternetPersent()){
+            splash();
+        }
+        else{
+            showAlertDialog(SplashScreenActivity.this, "Internet Connection",
+                    "You don't have internet connection", false);
+        }
+    }
 
-        splash();
+    private void showAlertDialog(Context context, String title, String message, Boolean status) {
+        AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+
+        alertDialog.setTitle(title);
+
+        alertDialog.setMessage(message);
+
+        alertDialog.setButton(-1,"RESTART", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+
+                finish();
+                startActivity(getIntent());
+            }
+        });
+
+        // Showing Alert Message
+        alertDialog.show();
+    }
+
+    private boolean isWorkingInternetPersent() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getBaseContext()
+                .getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (connectivityManager != null) {
+            NetworkInfo[] info = connectivityManager.getAllNetworkInfo();
+            if (info != null)
+                for (int i = 0; i < info.length; i++)
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        return true;
+                    }
+
+        }
+        return false;
     }
 
     private void splash() {
         Thread timerTread = new Thread() {
             public void run() {
                 try {
-                    sleep(1000);
+                    sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
